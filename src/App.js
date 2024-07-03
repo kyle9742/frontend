501136b4c77a1f4111037eb00e0362305b2829de
it <kyle9742@naver.com>
2024-07-03 11:45:29 +0900

    부트스트랩 테이블 샘플 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './layout/Home';
 import Navbar from './layout/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/js/bootstrap.bundle';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }

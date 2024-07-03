591af5db1699a30e908883e6106ae5681872683e
it <kyle9742@naver.com>
2024-07-03 11:40:49 +0900

    네브바 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './layout/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
     <div className="App">
-      start
+      <Navbar />
     </div>
   );
 }
